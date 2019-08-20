64972dacaffd62fcdea2fcd038478216641607bd
max ort <mortman2299@stu.d214.org>
2019-08-20 15:03:44 -0500

    mabey this

---
+++ WarmUp2_1.java
@@ -0,0 +1,41 @@
+/**
+ * Created by bob.brown on 8/23/16.
+ */
+public class WarmUp2_1 {
+    public static void main(String args[])
+    {
+        //On the line below, use a print or println statement to display the text below on the screen
+        // Hello WarmUps
+        System.out.println("Hello WarmUps \n");
+
+        //On the lines below, use a two println statements to display the text below on the screen
+        // Hello
+        // WarmUps
+        System.out.println("Hello ");
+        System.out.println("WarmUps \n");
+
+
+
+        //On the lines below, use a two print statements to display the text below on the screen
+        // Hello
+        // WarmUps
+        System.out.print("Hello \n");
+        System.out.print("WarmUps \n\n");
+
+
+
+        //On the lines below, use print statements and escape characters to display the text below
+        // 1
+        //      2
+        //            3
+
+        System.out.print("1 \n");
+        System.out.print("\t2 \n");
+        System.out.print("\t\t3 \n");
+
+
+
+
+
+    }
+}
