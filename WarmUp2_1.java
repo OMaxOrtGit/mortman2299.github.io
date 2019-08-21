9adae3901fd67472f7fc659add53bccb4cb2fa3c
Max Ort <43018776+mortman2299@users.noreply.github.com>
2019-08-21 08:02:30 -0500

    Update WarmUp2_1.java

--- WarmUp2_1.java
+++ WarmUp2_1.java
@@ -1,41 +1,8 @@
-/**
- * Created by bob.brown on 8/23/16.
- */
-public class WarmUp2_1 {
-    public static void main(String args[])
-    {
-        //On the line below, use a print or println statement to display the text below on the screen
-        // Hello WarmUps
-        System.out.println("Hello WarmUps \n");
+import java.applet.*;
+import java.awt.*;
 
-        //On the lines below, use a two println statements to display the text below on the screen
-        // Hello
-        // WarmUps
-        System.out.println("Hello ");
-        System.out.println("WarmUps \n");
-
-
-
-        //On the lines below, use a two print statements to display the text below on the screen
-        // Hello
-        // WarmUps
-        System.out.print("Hello \n");
-        System.out.print("WarmUps \n\n");
-
-
-
-        //On the lines below, use print statements and escape characters to display the text below
-        // 1
-        //      2
-        //            3
-
-        System.out.print("1 \n");
-        System.out.print("\t2 \n");
-        System.out.print("\t\t3 \n");
-
-
-
-
-
-    }
+public class HelloWorldApplet extends Applet {
+   public void paint (Graphics g) {
+      g.drawString ("Hello World", 25, 50);
+   }
 }
